aa8d4a11c917b4b9c5ab7c01d2b867c187d2cc43
manit20 <78215866+manit20@users.noreply.github.com>
2021-08-06 22:13:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.position(420, 200);
+    video = createCapture(VIDEO);
+    video.hide()
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
